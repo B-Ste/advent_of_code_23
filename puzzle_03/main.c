eeece3ca5cde06319106a97586881ea2552104fb
uykfh@student.kit.edu <benni.steeg@icloud.com>
2024-10-17 21:48:47 +0200

    solve part 2 of puzzle 3

--- puzzle_03/main.c
+++ puzzle_03/main.c
@@ -3,7 +3,60 @@
 #include<string.h>
 
 #define LINE_LENGTH 142
+#define NUMBER(x) ((x) >= 48 && (x) <= 57)
 #define SPECIAL_CHAR(x) ((x) != 0 && (x) != '.' && ((x) < 48 || (x) > 57))
+#define FREE(x) free(x); (x) = NULL;
+
+int *parseNumbers(char *line, int index) {
+    int *nums = (int *) malloc(3 * sizeof(int));
+    if (!nums) exit(1);
+    nums[1] = 1;
+    nums[2] = 1;
+    if (NUMBER(line[index])) {
+        int i = index;
+        int j = index;
+        while (i >= 0) {
+            if (!NUMBER(line[i])) break;
+            i--;
+        }
+        i++;
+        while (1) {
+            if (!NUMBER(line[j])) break;
+            j++;
+        }
+        j--;
+        char num[LINE_LENGTH] = {0};
+        memcpy(num, line + i, j - i + 1);
+        nums[0] = 1;
+        nums[1] = atoi(num);
+    } else {
+        if (index - 1 >= 0 && NUMBER(line[index - 1])) {
+            int i = index - 1;
+            while (i >= 0) {
+                if (!NUMBER(line[i])) break;
+                i--;
+            }
+            i++;
+            char num[LINE_LENGTH] = {0};
+            memcpy(num, line + i, index - i);
+            nums[0] = 1;
+            nums[1] = atoi(num);
+        }
+        if (NUMBER(line[index + 1])) {
+            int j = index + 1;
+            while (1) {
+                if (!NUMBER(line[j])) break;
+                j++;
+            }
+            j--;
+            char num[LINE_LENGTH] = {0};
+            memcpy(num, line + index + 1, j - index);
+            nums[0]++;
+            nums[2] = atoi(num);
+        }
+    }
+    return nums;
+}
 
 int main(int argc, char const *argv[])
 {
@@ -22,6 +75,7 @@ int main(int argc, char const *argv[])
 
     int done = 0;
     int sum_1 = 0;
+    int sum_2 = 0;
     while (1) {
         if (!fgets(line_below, LINE_LENGTH, f)) {
             for (int i = 0; i < LINE_LENGTH; i++) {
@@ -31,11 +85,14 @@ int main(int argc, char const *argv[])
         }
         line_below[LINE_LENGTH - 2] = 0;
 
-        // Calculates solution of part 1
         int left_bound = -1;
         int right_bound = -1;
         for (int i = 0; i < LINE_LENGTH - 1; i++) {
-            if (line[i] >= 48 && line[i] <= 57) {
+
+
+
+            // Calculates solution of part 1
+            if (NUMBER(line[i])) {
                 if (left_bound < 0) {
                     left_bound = i;
                     right_bound = i;
@@ -62,6 +119,29 @@ int main(int argc, char const *argv[])
                 left_bound = -1;
                 right_bound = -1;
             }
+
+
+
+            
+            // Calculates solution of part 2
+            if (line[i] == '*') {
+                int *num_above = parseNumbers(line_above, i);
+                int *num_curr = parseNumbers(line, i);
+                int *num_below = parseNumbers(line_below, i);
+                if (num_above[0] + num_curr[0] + num_below[0] == 2) {
+                    int gear_ratio = 1;
+                    gear_ratio *= num_above[1];
+                    gear_ratio *= num_above[2];
+                    gear_ratio *= num_curr[1];
+                    gear_ratio *= num_curr[2];
+                    gear_ratio *= num_below[1];
+                    gear_ratio *= num_below[2];
+                    sum_2 += gear_ratio;
+                }
+                FREE(num_above);
+                FREE(num_curr);
+                FREE(num_below);
+            }
         }
 
         if (done) break;
@@ -70,6 +150,7 @@ int main(int argc, char const *argv[])
     }
     
     printf("Solution 1: %i\n", sum_1);
+    printf("Solution 2: %i\n", sum_2);
 
     fclose(f);
     free(line_above);
